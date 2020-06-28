8f7aec3c7798a759c8c395a0a751a0141a810de5
juice-kuo <64064895+juice-kuo@users.noreply.github.com>
2020-06-28 12:09:09 +0800

    Update views.py

--- func3api/views.py
+++ func3api/views.py
@@ -8,6 +8,9 @@ from linebot.models import MessageEvent, TextMessage, PostbackEvent
 from module import func
 from urllib.parse import parse_qsl
 
+from super.models import company
+from django.db.models import Q
+
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
